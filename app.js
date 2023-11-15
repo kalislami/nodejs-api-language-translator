154af2097e4d9791be8a932cf6d2c49237f14644
LAMAK\Kamal <namaanekamal@gmail.com>
2023-11-16 05:17:18 +0700

    disable logging

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 import {web} from "./src/application/web.js";
-import {logger} from "./src/application/logging.js";
+// import {logger} from "./src/application/logging.js";
 
 const port = process.env.PORT || 3000;
 
 web.listen(port, () => {
-    logger.info(`app start on port: ${port}`);
+    console.log(`app start on port: ${port}`);
+    // logger.info(`app start on port: ${port}`);
 });
